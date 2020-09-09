0270fe3bbaca88aba897dacb249b5c657b13b05e
root <bensie_007@icloud.com>
2020-09-09 12:27:20 +0200

    Changed Project Settings

--- src/main/java/net/craftions/game/gui/Ingame.java
+++ src/main/java/net/craftions/game/gui/Ingame.java
@@ -4,12 +4,14 @@ import net.craftions.game.main.Main;
 
 import javax.swing.*;
 
+
 public class Ingame extends Screen{
 
     private JFrame f;
 
     @Override
     public void init() {
+
         this.f = new JFrame(Main.g.getName() + " " + Main.g.getVersion());
         this.f.setUndecorated(true);
         this.f.setExtendedState(JFrame.MAXIMIZED_BOTH);
